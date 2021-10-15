7a47a0bf1647b3008bcc8b85ff9ae52ec60e0963
marclloret <mlloretd@edu.tecnocampus.cat>
2021-10-15 12:29:45 +0200

    a

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 local ball = ball or require"C:/Users/bigth/Documents/Universitat/2on/T1/Programacio/Practiques/Prac1/LovePong/Practica 1 POO/src/ball.lua"
+local player = paddle or require("")
+local cpu = paddle or require("")
 
 function love.load(arg)
   if arg[#arg] == "-debug" then
@@ -6,7 +8,8 @@ function love.load(arg)
   end
   
   b=ball(0,0,10,300,0)
-    
+  player=paddle()
+  cpu=paddle()
 end
 
 function love.update(dt)
