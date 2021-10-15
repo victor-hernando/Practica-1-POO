d40f877f3c8bc3954a159ccf762eafebe73bbb13
marclloret <mlloretd@edu.tecnocampus.cat>
2021-10-15 12:48:47 +0200

    main

---
+++ src/score.lua
@@ -0,0 +1,18 @@
+local obj = obj or require("lib/object")
+local score = obj.extend()
+
+function score:new(points,x,y)
+  self.points=points
+  self.x=x
+  self.y=y
+  return self
+end
+
+function score:update(dt)
+
+end
+
+function score:draw()
+  love.graphics.print(self.points, self.x, self.y, 0, 1, 1, 0, 0, 0, 0)
+end
+
