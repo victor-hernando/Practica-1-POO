fd68f4827b40ff3f1847f9b1d588d3ecbcc8b8a7
victor-hernando <vhernando@edu.tecnocampus.cat>
2021-10-15 11:59:44 +0200

    ball

--- main.lua
+++ main.lua
@@ -1,9 +1,16 @@
+local ball = ball or require"src/ball.lua"
+
 function love.load()
-  ifarg[#arg] == "-debug" then
+  if arg[#arg] == "-debug" then
     require("moddebug").start()
+  end
+  
+  b=ball(0,0,10,300,0)
+    
 end
 
 function love.update()
+  
 end
 
 function love.draw()
--- src/ball.lua
+++ src/ball.lua
@@ -1,2 +1,25 @@
-local obj = obj or require("lib/object")
+local obj = obj or require"lib/object.lua"
 local ball = obj.extend()
+
+function ball:new(x,y,radi,velocitat,angle)
+  self.x=x
+  self.y=y
+  self.r=radi
+  self.vel=velocitat
+  self.ang=angle
+  --self.player=player
+  --self.cpu=cpu
+  return self
+end
+
+function ball:update(dt)
+  --Movemos la pelota
+  self.x = self.x + self.vel*math.cos(self.ang)*dt
+  self.y = self.y + self.vel*math.sin(self.ang)*dt
+end
+
+function ball:draw()
+  love.graphics.circle("fill", self.x, self.y, self.r)
+end
+
+return ball
