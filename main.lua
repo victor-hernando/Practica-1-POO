3ac567d5c0138609d4d6e948ba60f432bd4c9340
victor-hernando <vhernando@edu.tecnocampus.cat>
2021-10-18 17:30:22 +0200

    hola

--- main.lua
+++ main.lua
@@ -2,15 +2,22 @@ local data = data or require"data"
 local ball = ball or require"src/ball"
 local paddle = paddle or require"src/paddle"
 local score = score or require"src/score"
+--local button = buton or require  "src/button"
 local player, cpu
 local playerScore, cpuScore
 local pilota
+local start, exit
+local thingsToPrint
 
 function love.load(arg)
   if arg[#arg] == "-debug" then
     require("mobdebug").start()
   end
-  
+  --manageThings(start,true)
+  --start=button(200,h/2,50,20,"Start")
+  --exit=button(600,h/2,50,20,"Exit")
+  --table.insert(thingsToPrint,start)
+  --table.insert(thingsToPrint,exit)
   player = paddle(margeX,margeY-paddleHeight/2,paddleWidth,paddleHeight,paddleVel,true)
   cpu = paddle(w-(margeX+paddleWidth),margeY-paddleHeight/2,paddleWidth,paddleHeight,paddleVel*0.75,false)
   playerScore = score(initScore,playerScoreX,scoreY)
@@ -33,4 +40,17 @@ function love.draw()
   playerScore:draw()
   cpuScore:draw()
 end
+--[[
+function manageThings(thing, whatToDo)
+  if whatToDo == true then
+    table.insert(thingsToPrint,thing)
+  else
+    for i,v in ipairs(thingsToPrint) do
+      if v == thing then
+        table.remove(thingsToPrint,i)
+      end
+    end
+  end
+end
+--]]
 
