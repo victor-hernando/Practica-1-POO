e512eca663ff498472ef7eddb7a1d25d37925877
marclloret <mlloretd@edu.tecnocampus.cat>
2021-10-24 12:47:48 +0200

    Data

--- data.lua
+++ data.lua
@@ -1,19 +1,65 @@
 --Valors pantalla
 w, h = love.graphics.getDimensions() -- Get the screen width and height
-  
+
+--Sounds
+clickSound = love.audio.newSource("sounds/Click.wav", "static")
+soundtrack = love.audio.newSource("sounds/Soundtrack.mp3", "stream")
+colisionSound = love.audio.newSource("sounds/BallCollision.mp3", "static")
+scoreSound = love.audio.newSource("sounds/MakePoint.mp3", "static")
+loseSound = love.audio.newSource("sounds/LosePoint.mp3", "static")
+soundtrackVolume = 0.6
+scoreSoundVolume = 0.7
+
+--Sprites
+ballSprite = love.graphics.newImage("sprites/ball.png")
+playerSprite = love.graphics.newImage("sprites/paddle1.png")
+cpuSprite = love.graphics.newImage("sprites/paddle2.png")
+startButton = love.graphics.newImage("sprites/PlayButton.png")
+startButtonPressed = love.graphics.newImage("sprites/PlayButtonPressed.png")
+quitButton = love.graphics.newImage("sprites/QuitButton.png")
+quitButtonPressed = love.graphics.newImage("sprites/QuitButtonPressed.png")
+nameButton = love.graphics.newImage("sprites/NameButton.png")
+insertNameButton = love.graphics.newImage("sprites/InsertName.png")
+gameOver = love.graphics.newImage("sprites/GameOver.png")
+heart = love.graphics.newImage("sprites/Heart.png")
+backgroundSprite = love.graphics.newImage("sprites/background.png")
+planets = love.graphics.newImage("sprites/planets.png")
+
+--Buttons
+xFinish = w/2-gameOver:getWidth()/2.8
+yFinish = -62
+emptyText = " "
+finishScale = 0.7
+xName = w/2 - insertNameButton:getWidth()/2
+yName = 200
+xMeeting = w/2 - nameButton:getWidth()/2 
+yMeeting = 80
+xStart = 100
+yStart = h/2.35
+xExit = 490
+yExit = h/2.35
+
 --Valors pilota
 xBall = w/2
 yBall = h/2
 rBall = 10
 initBallVel = 200
 initBallAng = 0.3
+ballWidth = ballSprite:getWidth()
+ballHeight = ballSprite:getHeight()
 
 --Valors paddle
-margeX = 30
-margeY = h/2
-paddleWidth = 10
-paddleHeight = 100
-paddleVel = 150
+playerMargeX = 30 
+playerMargeY = h/2-playerSprite:getHeight()/2
+cpuMargeX = w-(30+cpuSprite:getWidth())
+cpuMargeY = h/2-cpuSprite:getHeight()/2
+
+paddleWidth = playerSprite:getWidth()
+paddleHeight = playerSprite:getHeight()
+playerVel = 150
+cpuWidth = cpuSprite:getWidth()
+cpuHeight = cpuSprite:getHeight()
+cpuVel = playerVel * 0.75
 
 --valors score
 initScore = 0
@@ -21,4 +67,13 @@ scoreY = 100
 playerScoreX = 130
 cpuScoreX = 525
 fontScale = 40
+
+--Lifes
+xLife = 60
+yLife = 30
+lifeScale = 0.065
+
+--
+cpuMaxPoints = 3
+maxLetters = 7
 
--- main.lua
+++ main.lua
@@ -12,61 +12,43 @@ local playerScore, cpuScore, playerLife
 local pilota, fons
 local start, exit, finish, name, meeting
 local thingsToPrint, thingsStart, thingsCatchName, thingsPlay, thingsHiScore
-local ballSprite, paddleSprite
-local startButton, startButtonPressed, quitButton, quitButtonPressed, nameButton, insertNameButton, gameOver, heart
 local writing = "return {"
-local clickSound, soundtrack, colisionSound, scoreSound, loseSound
 
 function love.load(arg)
   if arg[#arg] == "-debug" then
     require("mobdebug").start()
   end
   
-  clickSound = love.audio.newSource("sounds/Click.wav", "static")
-  soundtrack = love.audio.newSource("sounds/Soundtrack.mp3", "stream")
-  soundtrack:setVolume(0.6)
-  colisionSound = love.audio.newSource("sounds/BallCollision.mp3", "static")
-  scoreSound = love.audio.newSource("sounds/MakePoint.mp3", "static")
-  scoreSound:setVolume(0.8)
-  loseSound = love.audio.newSource("sounds/LosePoint.mp3", "static")
-  
-  ballSprite = love.graphics.newImage("sprites/ball.png")
-  playerSprite = love.graphics.newImage("sprites/paddle1.png")
-  cpuSprite = love.graphics.newImage("sprites/paddle2.png")
-  startButton = love.graphics.newImage("sprites/PlayButton.png")
-  startButtonPressed = love.graphics.newImage("sprites/PlayButtonPressed.png")
-  quitButton = love.graphics.newImage("sprites/QuitButton.png")
-  quitButtonPressed = love.graphics.newImage("sprites/QuitButtonPressed.png")
-  nameButton = love.graphics.newImage("sprites/NameButton.png")
-  insertNameButton = love.graphics.newImage("sprites/InsertName.png")
-  gameOver = love.graphics.newImage("sprites/GameOver.png")
-  heart = love.graphics.newImage("sprites/Heart.png")
+  soundtrack:setVolume(soundtrackVolume)
+  scoreSound:setVolume(scoreSoundVolume)
   
   thingsToPrint = {}
-  finish = tab(w/2-gameOver:getWidth()/2.8,-62,gameOver, " ", 0.7)
-  name = tab(w/2 - insertNameButton:getWidth()/2,200,insertNameButton)
-  meeting = tab(w/2 - nameButton:getWidth()/2 ,80,nameButton)
+  finish = tab(xFinish, yFinish, gameOver, emptyText, finishScale)
+  name = tab(xName, yName,insertNameButton)
+  meeting = tab(xMeeting, yMeeting, nameButton)
   thingsHiScore = {finish}
   thingsCatchName = {meeting,name}
   
-  start=button(100,h/2.35, startButton, startButtonPressed)
-  exit=button(490,h/2.35, quitButton, quitButtonPressed)
+  start=button(xStart, yStart, startButton, startButtonPressed)
+  exit=button(xExit, yExit, quitButton, quitButtonPressed)
   thingsStart={start,exit}
   
-  fons = background(love.graphics.newImage("sprites/background.png"), love.graphics.newImage("sprites/planets.png"))
+  fons = background(backgroundSprite, planets)
   
-  player = paddle(margeX,margeY-playerSprite:getHeight()/2,playerSprite:getWidth(),playerSprite:getHeight(),paddleVel,true, playerSprite)
-  cpu = paddle(w-(margeX+cpuSprite:getWidth()),margeY-cpuSprite:getHeight()/2,cpuSprite:getWidth(),cpuSprite:getHeight(),paddleVel*0.75,false, cpuSprite)
+  player = paddle(playerMargeX, playerMargeY, paddleWidth, paddleHeight, playerVel, true, playerSprite)
+  cpu = paddle(cpuMargeX, cpuMargeY, cpuWidth, cpuHeight, cpuVel, false, cpuSprite)
   
-  playerScore = score(initScore,playerScoreX,scoreY)
-  cpuScore = score(initScore,cpuScoreX,scoreY)
-  playerLife = lifes(60, 30, heart, cpuScore, 0.065, 0.065)
-  pilota = ball(xBall,yBall,ballSprite:getWidth(),ballSprite:getHeight(),initBallVel, initBallAng,player,cpu,playerScore,cpuScore, ballSprite, colisionSound, scoreSound, loseSound)
+  playerScore = score(initScore, playerScoreX, scoreY)
+  cpuScore = score(initScore, cpuScoreX, scoreY)
+  
+  playerLife = lifes(xLife, yLife, heart, cpuScore, lifeScale)
+  pilota = ball(xBall, yBall, ballWidth, ballHeight, initBallVel, initBallAng, player, cpu, playerScore, cpuScore, ballSprite, colisionSound, scoreSound, loseSound)
   
   cpu.setTarget(pilota)
   
   soundtrack:play()
-  thingsPlay = {player,cpu,playerScore,cpuScore,playerLife,pilota}
+  
+  thingsPlay = {player, cpu, playerScore, cpuScore, playerLife, pilota}
   thingsToPrint = thingsStart
 end
 
@@ -74,7 +56,7 @@ function love.update(dt)
   for i,v in ipairs(thingsToPrint) do
     v:update(dt)
   end
-  if cpuScore.points >= 3 then
+  if cpuScore.points >= cpuMaxPoints then
     updateScores(playerScore.name, playerScore.points)
     thingsToPrint = thingsHiScore
     cpuScore.points = 0
@@ -106,7 +88,7 @@ function love.textinput(text)
     local a = text
   if thingsToPrint == thingsCatchName then
     name.text = name.text..a
-    if #name.text >= 7 then
+    if #name.text >= maxLetters then
       thingsToPrint = thingsPlay
       playerScore.name = name.text
     end
